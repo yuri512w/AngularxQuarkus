5c6284e13592e02253de3891f79c59ea754d3566
yuri512w <=yuri512w@gmail.com>
2023-06-20 09:26:36 -0300

    MVP de Pessoas

--- src/app/pessoa-editar/pessoa-editar.component.ts
+++ src/app/pessoa-editar/pessoa-editar.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-pessoa-editar',
@@ -8,21 +9,37 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 })
 export class PessoaEditarComponent {
   pessoa: any;
+  pessoaData: any;
 
   constructor(
     public dialogRef: MatDialogRef<PessoaEditarComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private http: HttpClient
   ) {
     this.pessoa = { ...data.pessoa };
+
   }
 
   salvar(): void {
-    // Aqui você pode enviar os dados atualizados da pessoa para a API
-    // e implementar a lógica de tratamento de sucesso ou erro, se necessário
+    const { tipoPessoa, id, ...pessoaData } = this.pessoa;
+    this.pessoaData = pessoaData;
+    this.http.put(`http://localhost:8080/pessoas/${this.pessoa.id}`, this.pessoaData).subscribe(
+      () => {
+
+        console.log('Pessoa alterada com sucesso');
+        // Remover a pessoa da lista (se necessário)
+      },
+      error => {
+        console.error('Erro ao alterar pessoa', error);
+      }
+    );
+
+    // Aqui você pode implementar a lógica de tratamento de sucesso ou erro, se necessário
     // ...
 
     // Fechar o pop-up e retornar os novos dados da pessoa
     this.dialogRef.close(this.pessoa);
+
   }
 
   cancelar(): void {
